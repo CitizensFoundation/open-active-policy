e1cb6030a3df646d7eaa885045239845ed90a47b
Robert Bjarnason <robert.bjarnason@gmail.com>
2019-07-23 22:47:08 +0000

    Fixes

--- public/src/components/select-articles/oap-3d-budget.js
+++ public/src/components/select-articles/oap-3d-budget.js
@@ -512,7 +512,7 @@ class Oap3dBudget extends OapBaseElement {
     if (this.itemsInScene.length>5) {
       setTimeout(()=>{
         this.rotateTimeShift();
-        if (this.itemsInScene.length>5 && this.itemsInScene.length<7) {
+        if (this.itemsInScene.length>5 && this.itemsInScene.length<8) {
           this.fire('oap-play-sound-effect', 'oap_short_win_1');
         }
       }, 300);
