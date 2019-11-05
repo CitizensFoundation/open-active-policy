24ced5bd97e36c649bc80bbf753678de0b7a7808
Robert Bjarnason <robert.bjarnason@gmail.com>
2019-11-05 13:27:01 +0000

    Fixes

--- public/src/components/oap-bonuses-and-penalties.js
+++ public/src/components/oap-bonuses-and-penalties.js
@@ -4,7 +4,7 @@ const BONUS_FOR_HIGH = 9;
 const BONUS_FOR_MEDIUM = 5;
 const BONUS_FOR_LOW = 2;
 
-const PENALTY_FOR_HIGH = 8;
+const PENALTY_FOR_HIGH = 9;
 const PENALTY_FOR_MEDIUM = 5;
 const PENALTY_FOR_LOW = 2;
 
@@ -58,7 +58,7 @@ const saveItemToObject = (targetObject, item) => {
       targetObject.allPenaltiesByCulturalAttitute[item.attitute] = [];
     }
     if (!targetObject.sumPenaltiesByCulturalAttitute[item.attitute]) {
-      targetObject.sumPenaltiesByCulturalAttitute[item.attitute] = [];
+      targetObject.sumPenaltiesByCulturalAttitute[item.attitute] = 0;
     }
     targetObject.allPenaltiesByCulturalAttitute[item.attitute].push(item);
     targetObject.sumPenaltiesByCulturalAttitute[item.attitute]+=item.value;
